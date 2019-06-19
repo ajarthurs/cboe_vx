2c616e082c39473b3b4c90231b4c24e2a8de6e74
Aaron J Arthurs <aajarthurs@gmail.com>
2019-06-19 09:23:47 -0500

    Fix end-date calculation

--- post.py
+++ post.py
@@ -36,7 +36,7 @@ def main():
     # Setup timeframe to cover last several years from the most recent business day.
     years         = max(settings.st_years, settings.mt_years)
     now           = pd.to_datetime('now')
-    end_date      = now - cboe.bday_us*(not cboe.is_business_day(now)) - cboe.bday_us*(cboe.now < cboe.cboe_daily_update_time)
+    end_date      = now - cboe.bday_us*(not cboe.is_business_day(now)) - cboe.bday_us
     start_date    = end_date - years*365*cboe.Day()
     target_period = pd.date_range(start=start_date, end=end_date, freq=cboe.bday_us, normalize=True)
 
